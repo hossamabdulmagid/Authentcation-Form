a610cb76265070a09508ec586a3d263dc017924a
hossam <frontend@flows.sa>
2020-09-20 19:13:22 +0200

    update DarkMood

--- src/components/homepage.jsx
+++ src/components/homepage.jsx
@@ -1,11 +1,15 @@
 import React, { Fragment } from 'react';
-import { Flex, Box, Text, Tabs, Tab, TabList, TabPanel, TabPanels, Input, Button } from '@chakra-ui/core';
+import { Flex, Box, Text, Tabs, Tab, TabList, TabPanel, TabPanels, Input, Button, useColorMode, Icon } from '@chakra-ui/core';
 const HomePage = () => {
+    const { colorMode, toggleColorMode } = useColorMode();
     return (
         <Fragment className="App">
             <Flex height="100vh" align="center" justify="center">
                 <Box p="20px" backgroundColor="#C3C3C3" rounded="20px">
                     <Flex align="center" justify="center" direction="column">
+                        <Button position="absolute" top='5' left='5' rounded='50%' onClick={() => toggleColorMode()}>
+                            <Icon name={colorMode === "light" ? "moon" : "sun"} />
+                        </Button>
                         <Text fontSize="20px" >Authentcation Form</Text>
                         <Tabs mt="20px">
                             <TabList>
@@ -27,8 +31,8 @@ const HomePage = () => {
                                 </TabPanel>
                                 <TabPanel mt='15px'>
                                     <Flex align="center" justify="center" direction='column' >
-                                        <Input placeholder="FirstName" size="lg" type="text" mt="10px"/>
-                                        <Input placeholder="LastName" size="lg" type="text"mt="10px" />
+                                        <Input placeholder="FirstName" size="lg" type="text" mt="10px" />
+                                        <Input placeholder="LastName" size="lg" type="text" mt="10px" />
 
                                         <Input placeholder="Password" size="lg" type="password" mt="10px" />
                                         <Input placeholder="Confirm Password" size="lg" type="password" mt="10px" />

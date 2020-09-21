e3b19cb3c543c2eec904b2ae7b615a449e529315
hossam <frontend@flows.sa>
2020-09-21 20:06:49 +0200

    updated triger colormode

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -9,11 +9,11 @@ const LayOut = () => {
     const color = { light: "white", dark: "white.800" };
     return (
         <Grid templateColumns="repeat(5, 1fr)" gap={6} mb='200' bg={bgColor[colorMode]} color={color[colorMode]}>
-            <Box w="100%" h="10" bg="blue.800" p='1' rounded='15%' textAlign='center' >Hello</Box>
-            <Box w="100%" h="10" bg="blue.800" p='1' rounded='15%' textAlign='center' >From</Box>
-            <Box w="100%" h="10" bg="blue.800" p='1' rounded='15%' textAlign='center'>Charka</Box>
-            <Box w="100%" h="10" bg="blue.800" p='1' rounded='15%' textAlign='center'>Ui</Box>
-            <Box w="100%" h="10" bg="blue.800" p='1' rounded='15%' textAlign='center'>Components</Box>
+            <Box w="100%" h="10" bg="green.600" p='2' rounded='15%' textAlign='center' >Hello</Box>
+            <Box w="100%" h="10" bg="green.600" p='2' rounded='15%' textAlign='center' >From</Box>
+            <Box w="100%" h="10" bg="green.600" p='2' rounded='15%' textAlign='center'>Charka</Box>
+            <Box w="100%" h="10" bg="green.600" p='2' rounded='15%' textAlign='center'>Ui</Box>
+            <Box w="100%" h="10" bg="green.600" p='2' rounded='15%' textAlign='center'>Components</Box>
         </Grid>
     )
 };
--- src/components/trig.jsx
+++ src/components/trig.jsx
@@ -9,21 +9,27 @@ import {
     PopoverFooter,
     PopoverArrow,
     PopoverCloseButton,
-    Button
+    Button,
+    useColorMode
 } from "@chakra-ui/core";
-const Trig = () => (
-    <>
-        <Popover>
-            <PopoverTrigger>
-                <Button>Trigger</Button>
-            </PopoverTrigger>
-            <PopoverContent zIndex={4}>
-                <PopoverArrow />
-                <PopoverCloseButton />
-                <PopoverHeader>Confirmation!</PopoverHeader>
-                <PopoverBody>Are you sure you want to have that milkshake?</PopoverBody>
-            </PopoverContent>
-        </Popover>
-    </>
-);
+const Trig = () => {
+    const { colorMode, toggleColorMode } = useColorMode();
+
+    return (
+
+        <>
+            <Popover>
+                <PopoverTrigger>
+                    <Button variantColor={colorMode === 'light' ? "green" : 'pink'} >Trigger</Button>
+                </PopoverTrigger>
+                <PopoverContent zIndex={4}>
+                    <PopoverArrow />
+                    <PopoverCloseButton />
+                    <PopoverHeader>Confirmation You May Need help !</PopoverHeader>
+                    <PopoverBody>Are you sure you want read Ducemntation For  HelpCenter</PopoverBody>
+                </PopoverContent>
+            </Popover>
+        </>
+    )
+};
 export default Trig;
